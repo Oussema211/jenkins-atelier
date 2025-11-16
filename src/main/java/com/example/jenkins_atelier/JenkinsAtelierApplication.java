1e1440ffe8144cbd6ce1f14e7ab397d0424d002d
Oussema Haggui <ohaggui6@gmail.com>
2025-11-16 18:20:52 +0100

    first

---
+++ src/main/java/com/example/jenkins_atelier/JenkinsAtelierApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_atelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAtelierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAtelierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins_atelier/JenkinsAtelierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_atelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAtelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
